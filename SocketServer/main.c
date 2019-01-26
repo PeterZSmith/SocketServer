0a3f2478fc245a5d6ced662464f61c2f2a1a0f52
Peter <github@petes.fastmail.fm>
2019-01-27 09:40:11 +1100

    Initial Commit

---
+++ SocketServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  SocketServer
+//
+//  Created by Peter on 27/1/19.
+//  Copyright © 2019 Peter. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
